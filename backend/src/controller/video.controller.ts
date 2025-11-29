c4157a035d96b7438bd8c0cdcc43a0c4dbeb50b6
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-11-29 17:19:52 +0530

    Added segmentation, fragementation and created masterFile

--- backend/src/controller/video.controller.ts
+++ backend/src/controller/video.controller.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from "express";
+import { processVideoForHLS } from "../services/video.service";
+import  fs  from "fs";
 
 export const videoUploader= async (req: Request, res: Response) => {
   if(!req.file){ // we will the uploaded video on req.file
@@ -10,6 +12,29 @@ export const videoUploader= async (req: Request, res: Response) => {
   };
 
   const videoPath= req.file.path;
+  const outputPath= `output/${Date.now()}`;
+
+  processVideoForHLS(videoPath, outputPath, (err, masterPlaylistPath) => {
+    if(err) {
+      res.status(500).json({
+        msg: "An error occured while processing the video",
+        success: false
+      });
+      return;
+    };
+
+    fs.unlink(videoPath , (err) => {  // Deleting the file after uploading
+      if(err){
+        console.log("An error occured while deleting the video file ", err);
+      }
+    })
+    
+    res.status(200).json({
+      success: true,
+      msg: "Video processed successfully",
+      data: `/${masterPlaylistPath}`
+    });
+  })
 
   res.status(200).json({
     success: true,
